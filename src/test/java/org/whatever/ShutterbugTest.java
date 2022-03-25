e986c21ab193fb7bd98203ff53b6c9241134a6f2
Dennis Ryan <dryan@broadinstitute.org>
2022-03-25 12:52:31 -0400

    demo

--- src/test/java/org/whatever/ShutterbugTest.java
+++ src/test/java/org/whatever/ShutterbugTest.java
@@ -6,26 +6,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Iterator;
 
 @Test
 public class ShutterbugTest {
 
-
-    @DataProvider
-    public static Iterator<Object[]> webdriverProvider() {
-        return Arrays.asList(new Object[]{getWebDriver()}, new Object[]{getRemoteWebDriver()}).iterator();
-    }
-
-    @Test(dataProvider = "webdriverProvider")
-    public void testLocal(WebDriver webDriver) {
-
+    /**
+     * this case passes and creates a screenshot
+     */
+    public void testLocal() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
+        WebDriver webDriver = RemoteWebDriver.builder().addAlternative(new ChromeOptions()).build();
         try {
             webDriver.navigate().to("https://google.com");
             Path tempDirectory = new File("/tmp/test").toPath();
@@ -37,11 +31,23 @@ public class ShutterbugTest {
         }
     }
 
-    private static WebDriver getWebDriver() {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver");
-        return RemoteWebDriver.builder().addAlternative(new ChromeOptions()).build();
-    }
-    private static WebDriver getRemoteWebDriver() {
-        return RemoteWebDriver.builder().address("http://gpuigrid:4444").addAlternative(new ChromeOptions()).build();
+    /**
+     * this case fails and throws an exception
+     */
+    public void testRemote() {
+
+        // use a valid selenium grid url
+        WebDriver webDriver =
+                RemoteWebDriver.builder().address("http://gridaddress:4444")
+                        .addAlternative(new ChromeOptions()).build();
+        try {
+            webDriver.navigate().to("https://google.com");
+            Path tempDirectory = new File("/tmp/test").toPath();
+            Shutterbug.shootPage(webDriver, Capture.FULL).withName("foo").save(tempDirectory.toString());
+        } catch (Exception e) {
+            Assert.fail(e.getMessage());
+        } finally {
+            webDriver.quit();
+        }
     }
 }
